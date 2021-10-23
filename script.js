b539c819e750db0f111e15ca75ab4214d6e2b94f
David Rherinson <77796563+DavidRherinson@users.noreply.github.com>
2021-10-23 04:17:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const LinksSocialMedia = {
+  github: 'DavidRherinson',
+  youtube: 'rocketseat',
+  facebook: 'rocketseat',
+  instagram: 'david.rherinson',
+  twitter: 'rocketseat',
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfilesInfos(){
+  const url =`https://api.github.com/users/${LinksSocialMedia.github}`
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    userName.textContent = data.name
+    userBio.textContent = data.bio
+    userLink.href = data.html_url
+    userAvatar.src = data.avatar_url
+    userLogin.textContent = data.login
+  })
+}
+getGitHubProfilesInfos()
